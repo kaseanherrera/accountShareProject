8b5c2bfbca41f4bbfb769fd6424a67f74ef8a638
kaseanherrera <kaseanherrera@yahoo.com>
2015-12-11 08:49:49 -0800

    rigged back button

---
+++ AccountShare/Login.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace AccountShare
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Production = new System.Windows.Forms.CheckBox();
+            this.loginMessage = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(494, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(-1, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(609, 10);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Username:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Production
+            // 
+            this.Production.AutoSize = true;
+            this.Production.Location = new System.Drawing.Point(494, 166);
+            this.Production.Name = "Production";
+            this.Production.Size = new System.Drawing.Size(98, 21);
+            this.Production.TabIndex = 5;
+            this.Production.Text = "Production";
+            this.Production.UseVisualStyleBackColor = true;
+            // 
+            // loginMessage
+            // 
+            this.loginMessage.AutoSize = true;
+            this.loginMessage.BackColor = System.Drawing.SystemColors.Control;
+            this.loginMessage.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.loginMessage.Font = new System.Drawing.Font("Segoe WP SemiLight", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginMessage.Location = new System.Drawing.Point(12, -1);
+            this.loginMessage.Name = "loginMessage";
+            this.loginMessage.Size = new System.Drawing.Size(364, 38);
+            this.loginMessage.TabIndex = 6;
+            this.loginMessage.Text = "Step 1: Login to Salesforce ";
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(95, 98);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(497, 22);
+            this.userName.TabIndex = 25;
+            this.userName.TextChanged += new System.EventHandler(this.userName_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(95, 138);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(497, 22);
+            this.password.TabIndex = 26;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(456, 17);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Enter your Salesforce username and password                                      " +
+    "";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(-1, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(609, 10);
+            this.label6.TabIndex = 29;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 246);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.loginMessage);
+            this.Controls.Add(this.Production);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox Production;
+        private System.Windows.Forms.Label loginMessage;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ AccountShare/Login.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Salesforce.Common;
+using Salesforce.Force;
+using System.Dynamic;
+using System.Windows.Forms;
+using System.Text;
+using System.Web.Services.Protocols;
+using AccountShare.sforce;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+
+
+namespace AccountShare
+{
+    public partial class Login : Form
+    {
+
+        string instanceUrl;
+        static string AccessToken;
+        static string ApiVersion = "v34.0";
+        private SforceService binding;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(userName.Text.Length == 0 || password.Text.Length == 0){
+                     label4.Text = "Must enter a username and password!";
+                     return;
+                }
+
+                label4.Text = "Verifying salesforce username and password.";
+                bool gotIn = login(userName.Text, password.Text);
+               
+
+                if (gotIn) {
+                    label4.Text = "Success";
+                this.Hide();
+                new Form1(instanceUrl, AccessToken, ApiVersion).ShowDialog();
+                } 
+
+                
+    
+               
+
+            }
+            catch (Exception a)
+            {
+                //tell the user that something went wrong 
+                
+                Console.WriteLine(a.Message);
+                Console.WriteLine(a.StackTrace);
+
+                var innerException = a.InnerException;
+                while (innerException != null)
+                {
+                    Console.WriteLine(innerException.Message);
+                    Console.WriteLine(innerException.StackTrace);
+
+                    innerException = innerException.InnerException;
+                }
+            }
+
+            label4.Text = "Error logging in to Salesforce. Please check credentials.";
+
+         
+          
+
+        }
+
+        private bool login(string username, string password)
+        {
+  
+            // Create a service object
+            binding = new SforceService();
+            // Timeout after a minute
+            binding.Timeout = 60000;
+            // Try logging in
+            LoginResult lr;
+            try
+            {
+                lr = binding.login(username, password);
+            }
+            // ApiFault is a proxy stub generated from the WSDL contract when
+            // the web service was imported
+            catch (SoapException e)
+            {
+           
+                // Write the fault code to the console
+                Console.WriteLine(e.Code);
+                // Write the fault message to the console
+                Console.WriteLine("An unexpected error has occurred: " + e.Message);
+                // Write the stack trace to the console
+                Console.WriteLine(e.StackTrace);
+                // Return False to indicate that the login was not successful
+                return false;
+            }
+            // Check if the password has expired
+            if (lr.passwordExpired)
+            {
+                Console.WriteLine("An error has occurred. Your password has expired.");
+                return false;
+            }
+           
+            AccessToken = lr.sessionId;
+            String url = Regex.Split(lr.serverUrl, @"/services")[0];
+            instanceUrl = url;
+           
+            return true;
+        }
+
+
+        private void userName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
